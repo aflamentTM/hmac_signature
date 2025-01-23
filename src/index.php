37451e0f623b8b691ea6cb983bda1f2a4a3bb788
Arnaud Flament <arnaudflam@gmail.com>
2025-01-23 19:44:09 +0100

    in case of array form

--- src/index.php
+++ src/index.php
@@ -1,12 +1,29 @@
-<?php 
-
+<?php
+// if an array is used, the JSON_UNESCAPED_SLASHES and JSON_NUMERIC_CHECK flags can be used
+// $dataString = json_encode($dataArray, JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK); 
+/**
+ * And the html form will look like this:
+ *     <h1>API Signature Generator</h1>
+ * <form method="post">
+ *<label for="dataArray">Array Data (JSON format):</label><br>
+ *<textarea id="dataArray" name="dataArray" rows="10" cols="50"></textarea><br><br>
+ *
+ *       <label for="secretKey">Secret Key:</label><br>
+ *      <input type="text" id="secretKey" name="secretKey"><br><br>
+ *
+ *       <input type="submit" value="Generate Signature">
+ *  </form>
+ *</body>
+ *</html>
+ *
+ */
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $jsonData = $_POST['jsonData']; 
-    $secretKey = $_POST['secretKey']; 
+    $jsonData = $_POST['jsonData'];
+    $secretKey = $_POST['secretKey'];
 
     // Create a hash of the JSON data using HMAC-SHA256
-    $hash = hash_hmac('sha256', $jsonData, $secretKey, true); 
+    $hash = hash_hmac('sha256', $jsonData, $secretKey, true);
 
     // Base64 encode the hash
     $signature = base64_encode($hash);
@@ -17,27 +34,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 } else {
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>API Signature Generator</title>
-</head>
-<body>
+    <!DOCTYPE html>
+    <html>
 
-    <h1>API Signature Generator</h1>
+    <head>
+        <title>API Signature Generator</title>
+    </head>
 
-    <form method="post">
-        <label for="jsonData">JSON Data:</label><br>
-        <textarea id="jsonData" name="jsonData" rows="10" cols="50"></textarea><br><br>
+    <body>
 
-        <label for="secretKey">Secret Key:</label><br>
-        <input type="text" id="secretKey" name="secretKey"><br><br>
+        <h1>API Signature Generator</h1>
 
-        <input type="submit" value="Generate Signature">
-    </form>
+        <form method="post">
+            <label for="jsonData">JSON Data:</label><br>
+            <textarea id="jsonData" name="jsonData" rows="10" cols="50"></textarea><br><br>
 
-</body>
-</html>
+            <label for="secretKey">Secret Key:</label><br>
+            <input type="text" id="secretKey" name="secretKey"><br><br>
+
+            <input type="submit" value="Generate Signature">
+        </form>
+
+    </body>
+
+    </html>
 
 <?php
 }
